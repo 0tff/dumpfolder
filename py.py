915318dc84a8495668f2d4d9aa1ff0ee1f691788
0tff <162051924+0tff@users.noreply.github.com>
2024-06-14 11:20:36 +0200

    Create py.py

---
+++ py.py
@@ -0,0 +1,17 @@
+```
+\Downloads\libAlysseAndroid.so
+LOAD:00000000                               ; Format      : ELF for ARM (Shared object)
+LOAD:00000000                               ; Needed Library 'liblog.so'
+LOAD:00000000                               ; Needed Library 'libandroid.so'
+LOAD:00000000                               ; Needed Library 'libGLESv1_CM.so'
+LOAD:00000000                               ; Needed Library 'libEGL.so'
+LOAD:00000000                               ; Needed Library 'libm.so'
+LOAD:00000000                               ; Needed Library 'libz.so'
+LOAD:00000000                               ; Needed Library 'libGLESv2.so'
+LOAD:00000000                               ; Needed Library 'libdl.so'
+LOAD:00000000                               ; Needed Library 'libc.so'
+LOAD:00000000                               ; Shared Name 'libFluxusAndroid.so'
+LOAD:00000000                               ;
+LOAD:00000000                               ; Options     : EF_ARM_SOFT_FLOAT
+LOAD:00000000                               ; EABI version: 5
+```
